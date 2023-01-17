a690519f8b31d83f64cd999ef23206d7bde18502
kakasoo <kscodebase@gmail.com>
2023-01-17 17:28:11 +0900

    chore : separate test files

---
+++ src/test/from.spec.ts
@@ -0,0 +1,29 @@
+import { RegExpBuilder } from '../regexp-builder';
+
+describe('check from method work correctly.', () => {
+    it('from have to receive initial value ( = first parameter )', () => {
+        const current = new RegExpBuilder().from('from').currentExpression;
+        expect(current).toBe('from');
+    });
+
+    it('from paramter can be subRegExpBuilder', async () => {
+        const current = new RegExpBuilder().from((qb) => {
+            return qb.from('from').currentExpression;
+        }).currentExpression;
+
+        expect(current).toBe('from');
+    });
+
+    // TODO : fix it.
+    // it('cat in front of dog, dog in front of tree', () => {
+    //     const regexp = new RegExpBuilder()
+    //         .from((qb) => {
+    //             return qb.from('cat').include('dog', { isForehead: true });
+    //         })
+    //         .include('tree', { isForehead: true })
+    //         .getOne();
+
+    //     console.log(regexp);
+    //     expect(regexp.test('treedogcat')).toBe(true);
+    // });
+});
