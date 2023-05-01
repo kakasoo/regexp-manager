7387c9942728501e41cd8accbb965652fe756062
kakasoo <kscodebase@gmail.com>
2023-05-01 18:55:47 +0900

    feat : 기 구현된 타입을 정의

--- src/new-feature.ts
+++ src/new-feature.ts
@@ -2,18 +2,28 @@ type Merge<F, S> = {
     [K in keyof (F & S)]: K extends keyof S ? S[K] : K extends keyof F ? F[K] : never;
 };
 
-class Builder<T extends Record<PropertyKey, any> = {}> {
+class Builder<T extends Record<PropertyKey, any>> {
     private readonly source: T;
-    constructor(source: T) {
+    constructor(source: T = {} as Record<PropertyKey, any>) {
         this.source = source;
-    }
-
-    option<K extends PropertyKey, P>(key: K, value: P): Builder<Merge<Omit<T, K>, Record<K, P>>> {
-        const newSource = { ...this.source, [key]: value };
-        return new Builder<Omit<T, K> & Record<K, P>>(newSource);
     }
 
     get(): T {
         return this.source;
+    }
+
+    or(): any {}
+    and(): any {}
+    lessThan(): any {}
+    lessThanOrEqual(): any {}
+    moreThan(): any {}
+    moreThanOrEqual(): any {}
+    between(): any {}
+    isOptional(): any {}
+    include(): any {}
+
+    private option<K extends PropertyKey, P>(key: K, value: P): Builder<Merge<Omit<T, K>, Record<K, P>>> {
+        const newSource = { ...this.source, [key]: value };
+        return new Builder<Omit<T, K> & Record<K, P>>(newSource);
     }
 }
